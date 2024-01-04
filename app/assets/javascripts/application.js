91627a50ae09d5c70030db00b02fadb4580207ce
ruben <ruben.sanz.lanero@alumnos.upm.es>
2024-01-04 13:15:01 +0100

    .

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
@@ -15,6 +15,5 @@
 //= require jquery-ui
 //= require bootstrap
 //= require polyfills.js
-//= require query-string
 //= require search.js
 //= require_tree .
