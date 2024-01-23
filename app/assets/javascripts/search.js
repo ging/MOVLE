d738a9cc5e44da59c7be3abee2955fafcb06415a
ruben <ruben.sanz.lanero@alumnos.upm.es>
2024-01-23 21:39:58 +0100

    .

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -22,7 +22,12 @@ function randomOrder () {
   console.log('Random order')
   //Función para mostrar de manera aleatoria los item_box
   var finalResultsDiv = document.getElementById("final_results");
-  var itemBoxes = finalResultsDiv.getElementsByClassName("item_box");
+
+  if (finalResultsDiv.firstElementChild.id == "link_to_user") {
+    var itemBoxes = finalResultsDiv.getElementsByClassName("link_to_user");
+  } else if (finalResultsDiv.firstElementChild) {
+    var itemBoxes = finalResultsDiv.getElementsByClassName("item_box");
+  }
 
   // Convierte la colección de elementos en un array para poder mezclarlo
   var itemBoxesArray = Array.from(itemBoxes);
@@ -31,11 +36,6 @@ function randomOrder () {
   var shuffledItemBoxes = itemBoxesArray.sort(function() {
     return 0.5 - Math.random();
   });
-
-  // Vacía el contenedor de resultados
-  if (finalResultsDiv.firstChild) {
-    finalResultsDiv.removeChild(finalResultsDiv.firstChild);
-  }
 
   // Agrega los elementos mezclados al contenedor en el nuevo orden
   shuffledItemBoxes.forEach(function(itemBox) {
