d02dbbdd8a435938ba7e1db04ffe5a7d5c9f5c0e
ruben <ruben.sanz.lanero@alumnos.upm.es>
2024-01-24 10:40:14 +0100

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 	skip_authorization_check :only => [:frontpage,:home]
 
 	def frontpage
-		@popularItems = Presentation.public_items.sample(14)
+		@popularItems = Presentation.public_items.sample(12)
 		if user_signed_in?
 			redirect_to "/home"
 		else
@@ -15,10 +15,10 @@ class HomeController < ApplicationController
 
 	def home
 		publicItems = Presentation.public_items
-		@popularItems = publicItems.sort_by(&:views).reverse.take(7)
+		@popularItems = publicItems.sort_by(&:views).reverse.take(6)
 		@latestItems = publicItems.where("id NOT in (?)", @popularItems.map { |r| r.id })
-		@latestItems = @latestItems.sort_by(&:created_at).reverse.take(7)
-		@linksfiles= Link.public_items.sample(1) + Embed.public_items.sample(1) + Document.public_items.sample(5)
+		@latestItems = @latestItems.sort_by(&:created_at).reverse.take(6)
+		@linksfiles= Link.public_items.sample(1) + Embed.public_items.sample(1) + Document.public_items.sample(4)
 
 		respond_to do |format|
 			format.html { render layout: "application" }
